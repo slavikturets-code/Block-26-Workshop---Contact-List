703bd1de4c2bd6c5b9413197d31864cb9ac79087
svyatoslav turets <svyatoslav.turets@gmail.com>
2024-05-18 23:19:50 -0400

    Completed setting up the table tab

---
+++ Contact-List/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import ContactList from "./components/ContactList";
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
+
+
